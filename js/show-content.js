10c09cb5bbf3eb9853766370a7a293f8c7216773
fleipekkj13 <fleipekkj13@gmail.com>
2023-09-07 13:46:39 -0300

    some animations and text changes

--- js/show-content.js
+++ js/show-content.js
@@ -1,17 +1,21 @@
-const about_me = document.getElementById("about-me")
-
+const about_me = document.getElementById("about-me");
+const articleContent = document.getElementById("article");
 
 document.addEventListener("scroll", () =>{
-    
     const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
     console.log('Posição atual do scroll:', scrollTop);
 
     if(scrollTop >= 300){
         about_me.style.opacity = 1;
         about_me.style.transform = "translateY(0)"
+        
+        if(scrollTop >= 700){
+            articleContent.style.opacity = 1;
+            articleContent.style.transform = "translateY(0)"
+        }
     }
     else{
         about_me.style.opacity = 0;
+        articleContent.style.opacity = 0;
     }
-
 })
